dd9d89c5a752aaf975faf4697e1254ff2a7bbc96
Ega Fernanda Putra <dragoon.fernanda12@gmail.com>
2022-05-23 13:45:03 +0700

    aaaaaa
    dbmitra

---
+++ app/Http/Controllers/DBMitraController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DBMitraController extends Controller
+{
+    public function displaydb(){
+        return view('dbmitra');
+    }
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('dbmitra', 'DBMitraController@displaydb');
