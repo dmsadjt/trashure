2de251968e3e7d4c15d1d7b851eb2888c1223505
Raffi-072 <Priyadiantama98@gmail.com>
2022-05-25 14:02:51 +0700

    .

--- resources/views/pengguna/penggunadb.blade.php
+++ resources/views/pengguna/penggunadb.blade.php
@@ -1,13 +1,7 @@
+@extends('layouts.app')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pengguna Dashboard</title>
-</head>
+@section('content')
 <body>
     <h1>Welcome to pengguna Dashboard</h1>
 </body>
-</html>
+@endsection
