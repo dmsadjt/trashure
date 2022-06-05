e0287c5d934123fa521ee74968e8f8033452d19f
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2022-06-05 13:37:39 +0700

    profil model+migration

---
+++ database/seeds/ProfileSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProfileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('profils')->insert([
+            'user_id'=>'2',
+            'alamat'=>'Surabaya',
+            'no_rekening'=>'666777888',
+            'poin'=>'5000'
+        ]);
+
+        DB::table('profils')->insert([
+            'user_id'=>'3',
+            'alamat'=>'Sidoarjo',
+            'no_rekening'=>'111222333',
+            'kendaraan' => 'Honda CBR 250 RR',
+            'plat_kendaraan' => 'L 053 R',
+        ]);
+
+        DB::table('profils')->insert([
+            'user_id'=>'4',
+            'alamat'=>'Surabaya',
+            'no_rekening'=>'666777888',
+            'nama_bank' => 'Bank Sampah Vrindavan',
+            'status_bank'=> 'Active',
+        ]);
+
+    }
+}
