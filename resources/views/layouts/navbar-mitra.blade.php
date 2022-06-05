a7c2ca66d482309e249d633a9cfa7d789eb6bbb7
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2022-06-05 20:02:18 +0700

    update

---
+++ resources/views/layouts/navbar-mitra.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="crossorigin">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
+
+    <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="{{url('/css/mitra.css')}}">
+</head>
+
+<body>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="{{ url('/css/pengguna.css') }}">
+
+
+    <!-- Navbar -->
+    <nav class="navbar sticky-top navbar-expand-lg text-light">
+        <div class="container">
+            <a class="navbar-brand fw-bold text-light" href="/home">TRASHURE</a>
+            <ul class="navbar-nav justify-content-end align-items-center">
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">Pesanan</a>
+                </li>
+                <li class="dropdown-center">
+                    <a class="navbar-brand ms-3" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <span class="iconify" data-icon="ic:round-account-circle"></span>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                          document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+        </div>
+    </nav>
+    <div class="container">
+        @yield('content')
+    @show
+</div>
+</body>
+
+</html>
---
+++ resources/views/mitra/detail-pesanan.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.navbar-mitra')
+@section('title','Detail Pesanan')
+@section('content')
+<div class="mx-5 mt-3 rounded-3 p-5 text-white text-center bg-dark-green">
+    <h1>
+        <b>
+            Detail Pesanan
+        </b>
+    </h1>
+</div>
+<div class="mx-5 my-3 text-center">
+    <h2>
+        <b>
+        Order Masuk
+        </b>
+    </h2>
+</div>
+<div class="row mx-5 p-4 shadow-lg bg-green rounded-2">
+    <div class="col-8 p-4 bg-light-green me-auto">
+        <table>
+            <tr>
+                <td><b>Nama Pelanggan</b></td>
+                <td>idisan</td>
+            </tr>
+            <tr>
+                <td><b>Jenis Sampah</b> </td>
+                <td>idisan</td>
+            </tr>
+            <tr>
+                <td><b>Nomor</b> </td>
+                <td>idisan</td>
+            </tr>
+            <tr>
+                <td><b>Alamat</b></td>
+                <td>idisan</td>
+            </tr>
+        </table>
+    </div>
+    <div class="col-3 p-4 ms-auto bg-light-green">
+        <p>To : idisan</p>
+    </div>
+</div>
+<div class="text-center">
+    <button class="mt-5 shadow-lg btn btn-success">Terima pesanan</button>
+</div>
+@endsection
