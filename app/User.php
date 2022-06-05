70779bbd07d319365fda4a23553982b9dc0ad679
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2022-06-05 20:40:58 +0700

    +automatically create profile when regist

--- app/User.php
+++ app/User.php
@@ -37,6 +37,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    protected static function boot(){
+        parent::boot();
+
+        static::created(
+            function($user){
+                $user->profile()->create();
+            }
+        );
+    }
+
     public function roles()
     {
         return $this->belongsTo('App\Role');
--- database/seeds/ProfileSeeder.php
+++ database/seeds/ProfileSeeder.php
@@ -16,6 +16,7 @@ class ProfileSeeder extends Seeder
             'user_id'=>'2',
             'alamat'=>'Surabaya',
             'no_rekening'=>'666777888',
+            'no_telepon'=>'0888888881',
             'poin'=>'5000'
         ]);
 
@@ -23,6 +24,7 @@ class ProfileSeeder extends Seeder
             'user_id'=>'3',
             'alamat'=>'Sidoarjo',
             'no_rekening'=>'111222333',
+            'no_telepon'=>'0888888882',
             'kendaraan' => 'Honda CBR 250 RR',
             'plat_kendaraan' => 'L 053 R',
         ]);
@@ -31,6 +33,7 @@ class ProfileSeeder extends Seeder
             'user_id'=>'4',
             'alamat'=>'Surabaya',
             'no_rekening'=>'666777888',
+            'no_telepon'=>'0888888883',
             'nama_bank' => 'Bank Sampah Vrindavan',
             'status_bank'=> 'Active',
         ]);
