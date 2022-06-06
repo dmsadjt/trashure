cc56dad1149baaa872c1928628904ad2d78ff42b
Muhammad Dimas Adijanto <adijanto.205026@mhs.its.ac.id>
2022-06-06 11:35:40 +0700

    edit routing

--- app/Http/Controllers/Pesanan/PesananController.php
+++ app/Http/Controllers/Pesanan/PesananController.php
@@ -11,4 +11,23 @@ class PesananController extends Controller
         return view("pesanan.pembayaran");
     }
 
+    public function DetailPesanan(){
+        // $pesanan = DB::table('pesanans')->where('id', id)->get();
+
+        return view('mitra.detail-pesanan');
+    }
+
+
+    public function AmbilPesanan(){
+        // $pesanan = DB::table('pesanans')->where('id', id)->get();
+
+        return view('mitra.ambil-pesanan');
+    }
+
+
+    public function SelesaikanPesanan(){
+        // $pesanan = DB::table('pesanans')->where('id', id)->get();
+
+        return view('mitra.selesaikan-pesanan');
+    }
 }
